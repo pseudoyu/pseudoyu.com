fef5e1859fceee29e4d9c4047b74b839893005b6
pseudoyu <pseudoyu@connect.hku.hk>
2025-06-01 23:19:06 +0900

    fix: update PhotoGrid component to use object-contain for image display

--- src/components/photos/PhotoGrid.vue
+++ src/components/photos/PhotoGrid.vue
@@ -15,13 +15,13 @@ const photos = computed(() => {
 
 <template>
   <div class="photos grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4" max-w-500 mx-auto>
-    <div v-for="photo, idx in photos" :key="idx">
+    <div v-for="photo, idx in photos" :key="idx" class="flex items-center justify-center">
       <img
         :src="photo.url"
         :alt="photo.text || photo.name"
         :data-photo-index="idx"
         loading="lazy"
-        w-full object-cover
+        class="w-full h-auto object-contain"
       >
     </div>
   </div>
