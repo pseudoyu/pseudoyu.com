993ad36300d43e0c86bf9f3af4d66c3a6fc77303
pseudoyu <pseudoyu@connect.hku.hk>
2025-12-14 09:55:57 +0800

    feat: add weekly review 104 with reflections on work-life balance and personal growth

--- typed-router.d.ts
+++ typed-router.d.ts
@@ -32,6 +32,7 @@ declare module 'vue-router/auto-routes' {
     '/posts/weekly_review_101': RouteRecordInfo<'/posts/weekly_review_101', '/posts/weekly_review_101', Record<never, never>, Record<never, never>>,
     '/posts/weekly_review_102': RouteRecordInfo<'/posts/weekly_review_102', '/posts/weekly_review_102', Record<never, never>, Record<never, never>>,
     '/posts/weekly_review_103': RouteRecordInfo<'/posts/weekly_review_103', '/posts/weekly_review_103', Record<never, never>, Record<never, never>>,
+    '/posts/weekly_review_104': RouteRecordInfo<'/posts/weekly_review_104', '/posts/weekly_review_104', Record<never, never>, Record<never, never>>,
     '/posts/weekly_review_89': RouteRecordInfo<'/posts/weekly_review_89', '/posts/weekly_review_89', Record<never, never>, Record<never, never>>,
     '/posts/weekly_review_90': RouteRecordInfo<'/posts/weekly_review_90', '/posts/weekly_review_90', Record<never, never>, Record<never, never>>,
     '/posts/weekly_review_91': RouteRecordInfo<'/posts/weekly_review_91', '/posts/weekly_review_91', Record<never, never>, Record<never, never>>,
